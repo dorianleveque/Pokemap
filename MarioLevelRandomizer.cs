277f4c1294be288c36d70cf44c7fef06adc5c800
bosser <anne-gwenn.bosser>
2020-05-15 13:55:57 +0200

    initial commit

---
+++ MarioLevelRandomizer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Xml;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using System.Linq;
+
+namespace mariogenerator
+{
+    class MarioLevelRandomizer
+    {
+        public int Width {get; private set;}
+        public int Height {get; private set;}
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("fichier source: " + args[0]);
+            Console.WriteLine("fichier cible: " + args[1]);
+            var mlr = new MarioLevelRandomizer();
+            try
+            {
+                mlr.ShuffleLayers(args[0], args[1]);
+            }
+            catch(Exception e)
+            {
+               Console.WriteLine(e.ToString());
+               Environment.Exit(1);
+           }
+        }
+        private void ShuffleLayers(String sourceFile, String targetFile)
+        {
+            var xDocument = XDocument.Load(sourceFile);
+            var xMap = xDocument.Element("map");
+            Width = (int) xMap.Attribute("width");
+            Height = (int) xMap.Attribute("height");
+            foreach (var e in xMap.Elements().Where(x => x.Name == "layer"))
+            {
+                var tl = new TileLayer(e, Width, Height);
+                tl.ShuffleColumns();
+            }
+            xDocument.Save(targetFile);
+        }
+    }
+}
---
+++ Samples/Terrain.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="Terrain (16x16)" tilewidth="16" tileheight="16" tilecount="242" columns="22">
+ <image source="Terrain.png" width="352" height="176"/>
+</tileset>
